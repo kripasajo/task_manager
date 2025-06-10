33cda857f6ae4523334fd516c3304a9078c09a9e
kripasajo200@gmail.com <kripa sajo>
2025-06-10 15:00:51 +0530

    Starting the creation of db

---
+++ task_manager/starter/db/connect.js
@@ -0,0 +1,4 @@
+const mongoose = require('mongoose')
+
+const connectionString =
+ 'mongodb+srv://kripasajo:%23kripa%402005@taskmanager.ehxtbgz.mongodb.net/?retryWrites=true&w=majority&appName=TaskManager'
